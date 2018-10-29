7545d2235b5373a75e2728516c12031dd6bbaabf
FelicianoSurya <suryamarcello@gmail.com>
2018-10-29 13:34:58 +0700

    .

---
+++ Design/login.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Login Page</title>
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"></script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+	<link rel="stylesheet" type="text/css" href="../asset/css/login.css">
+</head>
+<body class="body">
+<div class="row">
+	<div class="col-lg-4"></div>
+	<div class="col-lg-4 kotak">
+		<p class="judul">GoBook</p>
+	</div>
+	<div class="col-lg-4"></div>
+</div>
+</body>
+</html>
