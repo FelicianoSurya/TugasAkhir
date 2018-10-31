b3c6b50c01f56be64720586157b7aaac895d3ec9
David Leonardo <davidleonardo621@gmail.com>
2018-10-31 11:04:43 +0700

    .

---
+++ Design/homeweb.php
@@ -0,0 +1,118 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"></script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+</head>
+<style type="text/css">
+	.center{
+		text-align: center;
+	}
+	.header{
+		font-weight: bold;
+		font-size: 35px;
+	}
+	.head{
+		font-size: 25px;
+	}
+	.font1{
+		font-weight: bold;
+		font-size: 22px;
+	}
+	.font2{
+		color: grey;
+		font-size: 20px;
+	}
+	.top{
+		margin-top: 15px;
+	}
+	.background{
+		margin-top: 10px;
+		background-image: url('../asset/images/background.jpg');
+		height: 350px;
+		width: 100%;
+	}
+	.background1{ 
+		padding: 0px 25px 20px 25px;
+		background-image: linear-gradient(to right, darkgrey, #4f575f);
+	}
+	.content{
+		font-size: 45px;
+		color: white;
+		margin-top: 50px;
+	}
+	.button{
+		color: white;
+		background-color: #d83b3b;
+		width: 100%;
+		font-size: 25px;
+		margin-top: 40px;
+	}
+	.button1{
+		color: yellow;
+		background-color: #d83b3b;
+		border: none;
+		font-size: 20px;
+		width: 120px;
+	}
+	.text{
+		color: black;
+		font-size: 30px;
+	}
+	.border{
+		border : 1px solid black;
+		background-image: linear-gradient(to bottom, white, grey, darkgrey, black);
+	}
+	.jarak{
+		margin: 15px 0px;
+	}
+</style>
+<body>
+	<div class="container-fluid">
+		<div class="row">
+			<div class="col-lg-12 header center">Go Book</div>
+		</div>
+		<div class="row">
+			<div class="col-lg-12 head center">Easily To Find Book</div>
+		</div>
+		<div class="row center top">
+			<div class="col-lg-3 font1"><u>Home</u></div>
+			<div class="col-lg-3 font2">Download</div>
+			<div class="col-lg-3 font2">Shop</div>
+			<div class="col-lg-3 font2">Profile</div>
+		</div>
+		<div class="background">
+			<div class="row">
+				<div class="col-lg-2"></div>
+				<div class="col-lg-8 center content">Discover great book to read<br>find in goBook</div>
+				<div class="col-lg-2"></div>
+			</div>
+			<div class="row">
+				<div class="col-lg-4"></div>
+				<div class="col-lg-4"><button class="button center">Find Out</button></div>
+				<div class="col-lg-4"></div>
+			</div>
+		</div>
+		<div class="background1">
+			<div class="row">
+				<div class="col-lg-12 text">Favourite</div>
+			</div>
+			<div class="border">
+				<div class="row jarak center">
+					<div class="col-lg-1"></div>
+					<div class="col-lg-2"><img src="../asset/images/buku1.jpg" style="width: 80%;"><br><br><button class="button1">See More</button></div>
+					<div class="col-lg-2"><img src="../asset/images/buku1.jpg" style="width: 80%;"><br><br><button class="button1">See More</button></div>
+					<div class="col-lg-2"><img src="../asset/images/buku1.jpg" style="width: 80%;"><br><br><button class="button1">See More</button></div>
+					<div class="col-lg-2"><img src="../asset/images/buku1.jpg" style="width: 80%;"><br><br><button class="button1">See More</button></div>
+					<div class="col-lg-2"><img src="../asset/images/buku1.jpg" style="width: 80%;"><br><br><button class="button1">See More</button></div>
+					<div class="col-lg-1"></div>
+				</div>
+			</div>
+		</div>
+	</div>
+</body>
+</html>
