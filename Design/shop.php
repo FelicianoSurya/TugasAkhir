251fd4f264b80d36131cabbb71f6d946e81eeb8d
David Leonardo <davidleonardo621@gmail.com>
2019-01-10 22:11:21 +0700

    .

--- Design/shop.php
+++ Design/shop.php
@@ -7,30 +7,34 @@
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"></script>
 	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>
 	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-	<link rel="stylesheet" type="text/css" href="../Asset/css/shop.css">
+	<link rel="stylesheet" type="text/css" href="../Asset/css/shop1.css">
 </head>
 <body>
 	<div class="container-fluid-fullscreen">
 		<div class="row">
-			<div class="col-lg-12 header text-center top">GoBook</div>
+			<div class="col-lg-12 header text-center">Go Book</div>
 		</div>
 		<div class="row">
-			<div class="col-lg-12 head text-center"><b>Find <span style="color:#095a5f;">Your Book</span> Easily</b></div>
+			<div class="col-lg-12 head text-center">Find Your Book Easily</div>
 		</div>
-		<div class="row text-center top1">
-			<div class="col-lg-3 font2">
-				<a href="homeweb.php">HOME</a>
+		<ul class="topnav">
+			<div class="row text-center top">
+				<div class="col-lg-3 font2"><li><a href="homeweb.php">Home</a></li></div>
+				<div class="col-lg-3 font2"><li><a href="down.php">Download</a></li></div>
+				<div class="col-lg-3 font1"><li><a href="shop.php"><u>Shop</u></a></li></div>
+				<div class="col-lg-2 font2"><li><a href="profileweb.php">Profile</a></li></div>
+				<div class="col-lg-1">
+					<li class="icon hilang">
+  						<a href="javascript:void(0);" onclick="myFunction()">☰</a>
+ 					</li>
+				</div>
 			</div>
-			<div class="col-lg-3 font2">
-				<a href="down.php">DOWNLOAD</a>
-			</div>
-			<div class="col-lg-3 font1">
-				<a href="shop.php"><u>SHOP</u></a>
-			</div>
-			<div class="col-lg-3 font2">
-				<a href="profileweb.php">PROFILE</a>
-			</div>
-		</div>
+		</ul>
+		<script>
+			function myFunction() {
+ 				document.getElementsByClassName("topnav")[0].classList.toggle("responsive");
+			}
+		</script>
 		<div class="row">
 			<div class="col-lg-12 tabel">
 				<div class="row">
