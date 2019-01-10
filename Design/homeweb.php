d447833f27f275cf743ecb65566cc4d312cea1c8
Zauhiung <emmanuel.karyaten@gmail.com>
2019-01-10 21:12:28 +0700

    .

--- Design/homeweb.php
+++ Design/homeweb.php
@@ -7,7 +7,7 @@
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"></script>
 	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>
 	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-	<link rel="stylesheet" type="text/css" href="../Asset/css/home1.css">
+	<link rel="stylesheet" type="text/css" href="../Asset/css/home.css">
 </head>
 <body>
 	<div class="container-fluid-fullscreen">
@@ -18,35 +18,65 @@
 			<div class="col-lg-12 head text-center"><b>Find <span style="color:#095a5f;">Your Book</span> Easily</b></div>
 		</div>
 		<div class="row text-center top">
-			<div class="col-lg-3 font1"><a href="homeweb.php"><u>HOME</u></a></div>
-			<div class="col-lg-3 font2"><a href="down.php">DOWNLOAD</a></div>
-			<div class="col-lg-3 font2"><a href="shop.php">SHOP</a></div>
-			<div class="col-lg-3 font2"><a href="profileweb.php">PROFILE</a></div>
+			<div class="col-lg-3 font1">
+				<a href="homeweb.php"><u>HOME</u></a>
+			</div>
+			<div class="col-lg-3 font2">
+				<a href="down.php">DOWNLOAD</a>
+			</div>
+			<div class="col-lg-3 font2">
+				<a href="shop.php">SHOP</a>
+			</div>
+			<div class="col-lg-3 font2">
+				<a href="profileweb.php">PROFILE</a>
+			</div>
 		</div>
 		<div class="background">
-			<div class="row">
-				<div class="col-lg-2"></div>
-				<div class="col-lg-8 text-center content"><b>Discover great book to read<br>Find in GoBook</b></div>
-				<div class="col-lg-2"></div>
+			<div class="row kotak">
+				<div class="col-lg-12 text-center content">
+					<b>Discover great book to read<br>Find in GoBook</b>
+				</div>
 			</div>
 			<div class="row">
 				<div class="col-lg-4"></div>
-				<div class="col-lg-4"><button class="btn button text-center">Search</button></div>
+				<div class="col-lg-4">
+					<button class="btn button text-center">Search</button>
+				</div>
 				<div class="col-lg-4"></div>
 			</div>
 		</div>
 		<div class="background1">
 			<div class="row">
-				<div class="col-lg-12 text fav"><b><img src="../Asset/images/heart.png" width="30px">Favourite</b></div>
+				<div class="col-lg-12 text fav"><b><img src="../Asset/images/heart.png" width="30px" class="imgpad">Favourite</b></div>
 			</div>
-			<div class="border">
+			<div class="row">
 				<div class="row jarak text-center">
 					<div class="col-lg-1"></div>
-					<div class="col-lg-2"><img src="../asset/images/book1.jpg" style="width: 80%;"><br><br><button class="button1">See More</button></div>
-					<div class="col-lg-2"><img src="../asset/images/book2.jpg" style="width: 80%;"><br><br><button class="button1">See More</button></div>
-					<div class="col-lg-2"><img src="../asset/images/book3.jpg" style="width: 82%;"><br><br><button class="button1">See More</button></div>
-					<div class="col-lg-2"><img src="../asset/images/book4.jpg" style="width: 85%;"><br><br><button class="button1">See More</button></div>
-					<div class="col-lg-2"><img src="../asset/images/book5.jpg" style="width: 85%;"><br><br><button class="button1">See More</button></div>
+						<div class="col-lg-2">
+							<img src="../asset/images/book1.jpg" style="width: 80%;"><br><br>
+							<p>Jaroslav Kalfar</p>
+							<button class="button1">See More</button>
+						</div>
+						<div class="col-lg-2">
+							<img src="../asset/images/book2.jpg" style="width: 80%;"><br><br>
+							<p>Light Shining in the Forest</p>
+							<button class="button1">See More</button>
+						</div>
+						<div class="col-lg-2">
+							<img src="../asset/images/book3.jpg" style="width: 82%;"><br><br>
+							<p>After Alice</p>
+							<button class="button1">See More</button>
+						</div>
+						<div class="col-lg-2">
+							<img src="../asset/images/book4.jpg" style="width: 85%;"><br><br>
+							<p>The River and the Book</p>
+							<button class="button1">See More</button>
+						</div>
+						<div class="col-lg-2">
+							<img src="../asset/images/book5.jpg" style="width: 85%;"><br><br>
+							<p>Freedom is Space for the Spirit</p>
+							<button class="button1">See More</button>
+						</div>
 					<div class="col-lg-1"></div>
 				</div>
 			</div>
