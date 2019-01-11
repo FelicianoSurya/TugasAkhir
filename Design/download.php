446f8a55694cf8fc00a2b7d03464eda9c0c1263c
FelicianoSurya <suryamarcello@gmail.com>
2019-01-11 13:32:11 +0700

    .

--- Design/download.php
+++ Design/download.php
@@ -19,7 +19,7 @@
 		</div>
 		<div class="row text-center top">
 			<div class="col-lg-3 font2"><a href="homeweb.php">Home</a></div>
-			<div class="col-lg-3 font1"><a href="down.php"><u>Download</u></a></div>
+			<div class="col-lg-3 font2"><a href="down.php">Download</a></div>
 			<div class="col-lg-3 font2"><a href="shop.php">Shop</a></div>
 			<div class="col-lg-3 font2"><a href="profileweb.php">Profile</a></div>
 		</div>
