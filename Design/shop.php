d32cb3c72faac75b8bb984d6e843c5313b9155f8
FelicianoSurya <suryamarcello@gmail.com>
2019-01-11 12:56:29 +0700

    .

--- Design/shop.php
+++ Design/shop.php
@@ -7,7 +7,7 @@
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"></script>
 	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>
 	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-	<link rel="stylesheet" type="text/css" href="../Asset/css/shop1.css">
+	<link rel="stylesheet" type="text/css" href="../Asset/css/shop.css">
 </head>
 <body>
 	<div class="container-fluid-fullscreen">
@@ -36,91 +36,245 @@
 				<div class="row">
 					<div class="col-lg-12 tabel1">
 						<div class="row top">
-							<div class="col-lg-2 box jarak text-center">
+							<div class="box jarak text-center">
+								<div class="image-box">
 								<img src="../Asset/images/fantasy1.jpg" class="gambar">
+								</div>
+								<div class="topic-book">
 								<p class="book">Buku Pintar</p>
-								<p style="font-weight: bold; margin: auto; font-size: 20px;">50
-								<img src="../Asset/images/coin.png" width="10%;" class="koin"></p>
-								<button class="buton">Buy</button>
+								</div>
+								<div class="book-prize">
+								<p>50
+								<img src="../Asset/images/coin.png" width="14%;" class="koin"></p>
+								</div>
+								<div class="book-buy">
+								<button class="btn btn-primary buton">Buy</button>
+								</div>
 							</div>
-							<div class="col-lg-2 box jarak text-center">
+							<div class="box jarak text-center">
+								<div class="image-box">
 								<img src="../Asset/images/fantasy2.jpg" class="gambar">
+								</div>
+								<div class="topic-book">
 								<p class="book">Buku Pintar</p>
-								<p style="font-weight: bold; margin: auto; font-size: 20px;">50
-								<img src="../Asset/images/coin.png" width="10%;" class="koin"></p>
-								<button class="buton">Buy</button>
+								</div>
+								<div class="book-prize">
+								<p>50
+								<img src="../Asset/images/coin.png" width="14%;" class="koin"></p>
+								</div>
+								<div class="book-buy">
+								<button class="btn btn-primary buton">Buy</button>
+								</div>
 							</div>
-							<div class="col-lg-2 box jarak text-center">
+							<div class="box jarak text-center">
+								<div class="image-box">
 								<img src="../Asset/images/fantasy3.jpg" class="gambar">
+								</div>
+								<div class="topic-book">
 								<p class="book">Buku Pintar</p>
-								<p style="font-weight: bold; margin: auto; font-size: 20px;">50
-								<img src="../Asset/images/coin.png" width="10%;" class="koin"></p>
-								<button class="buton">Buy</button>
+								</div>
+								<div class="book-prize">
+								<p>50
+								<img src="../Asset/images/coin.png" width="14%;" class="koin"></p>
+								</div>
+								<div class="book-buy">
+								<button class="btn btn-primary buton">Buy</button>
+								</div>
 							</div>
-							<div class="col-lg-2 box jarak text-center">
+							<div class="box jarak text-center">
+								<div class="image-box">
 								<img src="../Asset/images/fantasy4.jpg" class="gambar">
+								</div>
+								<div class="topic-book">
 								<p class="book">Buku Pintar</p>
-								<p style="font-weight: bold; margin: auto; font-size: 20px;">50
-								<img src="../Asset/images/coin.png" width="10%;" class="koin"></p>
-								<button class="buton">Buy</button>
+								</div>
+								<div class="book-prize">
+								<p>50
+								<img src="../Asset/images/coin.png" width="14%;" class="koin"></p>
+								</div>
+								<div class="book-buy">
+								<button class="btn btn-primary buton">Buy</button>
+								</div>
 							</div>
-							<div class="col-lg-2 box jarak text-center">
+							<div class="box jarak text-center">
+								<div class="image-box">
 								<img src="../Asset/images/fantasy5.jpg" class="gambar">
+								</div>
+								<div class="topic-book">
 								<p class="book">Buku Pintar</p>
-								<p style="font-weight: bold; margin: auto; font-size: 20px;">50
-								<img src="../Asset/images/coin.png" width="10%;" class="koin"></p>
-								<button class="buton">Buy</button>
+								</div>
+								<div class="book-prize">
+								<p>50
+								<img src="../Asset/images/coin.png" width="14%;" class="koin"></p>
+								</div>
+								<div class="book-buy">
+								<button class="btn btn-primary buton">Buy</button>
+								</div>
 							</div>
-							<div class="col-lg-2 box jarak text-center">
+							<div class="box jarak text-center">
+								<div class="image-box">
 								<img src="../Asset/images/fantasy6.jpg" class="gambar">
+								</div>
+								<div class="topic-book">
 								<p class="book">Buku Pintar</p>
-								<p style="font-weight: bold; margin: auto; font-size: 20px;">50
-								<img src="../Asset/images/coin.png" width="10%;" class="koin"></p>
-								<button class="buton">Buy</button>
+								</div>
+								<div class="book-prize">
+								<p>50
+								<img src="../Asset/images/coin.png" width="14%;" class="koin"></p>
+								</div>
+								<div class="book-buy">
+								<button class="btn btn-primary buton">Buy</button>
+								</div>
 							</div>
-						</div>
-						<div class="row top1">
-							<div class="col-lg-2 box jarak text-center">
+							<div class="box jarak text-center">
+								<div class="image-box">
 								<img src="../Asset/images/fantasy6.jpg" class="gambar">
+								</div>
+								<div class="topic-book">
 								<p class="book">Buku Pintar</p>
-								<p style="font-weight: bold; margin: auto; font-size: 20px;">50
-								<img src="../Asset/images/coin.png" width="10%;" class="koin"></p>
-								<button class="buton">Buy</button>
+								</div>
+								<div class="book-prize">
+								<p>50
+								<img src="../Asset/images/coin.png" width="14%;" class="koin"></p>
+								</div>
+								<div class="book-buy">
+								<button class="btn btn-primary buton">Buy</button>
+								</div>
 							</div>
-							<div class="col-lg-2 box jarak text-center">
+							<div class="box jarak text-center">
+								<div class="image-box">
 								<img src="../Asset/images/fantasy7.jpg" class="gambar">
+								</div>
+								<div class="topic-book">
 								<p class="book">Buku Pintar</p>
-								<p style="font-weight: bold; margin: auto; font-size: 20px;">50
-								<img src="../Asset/images/coin.png" width="10%;" class="koin"></p>
-								<button class="buton">Buy</button>
+								</div>
+								<div class="book-prize">
+								<p>50
+								<img src="../Asset/images/coin.png" width="14%;" class="koin"></p>
+								</div>
+								<div class="book-buy">
+								<button class="btn btn-primary buton">Buy</button>
+								</div>
 							</div>
-							<div class="col-lg-2 box jarak text-center">
+							<div class="box jarak text-center">
+								<div class="image-box">
 								<img src="../Asset/images/fantasy8.jpg" class="gambar">
+								</div>
+								<div class="topic-book">
 								<p class="book">Buku Pintar</p>
-								<p style="font-weight: bold; margin: auto; font-size: 20px;">50
-								<img src="../Asset/images/coin.png" width="10%;" class="koin"></p>
-								<button class="buton">Buy</button>
+								</div>
+								<div class="book-prize">
+								<p>50
+								<img src="../Asset/images/coin.png" width="14%;" class="koin"></p>
+								</div>
+								<div class="book-buy">
+								<button class="btn btn-primary buton">Buy</button>
+								</div>
 							</div>
-							<div class="col-lg-2 box jarak text-center">
+							<div class="box jarak text-center">
+								<div class="image-box">
 								<img src="../Asset/images/fantasy9.jpg" class="gambar">
+								</div>
+								<div class="topic-book">
 								<p class="book">Buku Pintar</p>
-								<p style="font-weight: bold; margin: auto; font-size: 20px;">50
-								<img src="../Asset/images/coin.png" width="10%;" class="koin"></p>
-								<button class="buton">Buy</button>
+								</div>
+								<div class="book-prize">
+								<p>50
+								<img src="../Asset/images/coin.png" width="14%;" class="koin"></p>
+								</div>
+								<div class="book-buy">
+								<button class="btn btn-primary buton">Buy</button>
+								</div>
 							</div>
-							<div class="col-lg-2 box jarak text-center">
+							<div class="box jarak text-center">
+								<div class="image-box">
 								<img src="../Asset/images/fantasy10.jpg" class="gambar">
+								</div>
+								<div class="topic-book">
 								<p class="book">Buku Pintar</p>
-								<p style="font-weight: bold; margin: auto; font-size: 20px;">50
-								<img src="../Asset/images/coin.png" width="10%;" class="koin"></p>
-								<button class="buton">Buy</button>
+								</div>
+								<div class="book-prize">
+								<p>50
+								<img src="../Asset/images/coin.png" width="14%;" class="koin"></p>
+								</div>
+								<div class="book-buy">
+								<button class="btn btn-primary buton">Buy</button>
+								</div>
 							</div>
-							<div class="col-lg-2 box jarak text-center">
-								<img src="../Asset/images/fantasy11.jpg" class="gambar">
+							<div class="box jarak text-center">
+								<div class="image-box">
+								<img src="../Asset/images/fantasy10.jpg" class="gambar">
+								</div>
+								<div class="topic-book">
 								<p class="book">Buku Pintar</p>
-								<p style="font-weight: bold; margin: auto; font-size: 20px;">50
-								<img src="../Asset/images/coin.png" width="10%;"></p>
-								<button class="buton">Buy</button>
+								</div>
+								<div class="book-prize">
+								<p>50
+								<img src="../Asset/images/coin.png" width="14%;" class="koin"></p>
+								</div>
+								<div class="book-buy">
+								<button class="btn btn-primary buton">Buy</button>
+								</div>
+							</div>
+							<div class="box jarak text-center">
+								<div class="image-box">
+								<img src="../Asset/images/fantasy10.jpg" class="gambar">
+								</div>
+								<div class="topic-book">
+								<p class="book">Buku Pintar</p>
+								</div>
+								<div class="book-prize">
+								<p>50
+								<img src="../Asset/images/coin.png" width="14%;" class="koin"></p>
+								</div>
+								<div class="book-buy">
+								<button class="btn btn-primary buton">Buy</button>
+								</div>
+							</div>
+							<div class="box jarak text-center">
+								<div class="image-box">
+								<img src="../Asset/images/fantasy10.jpg" class="gambar">
+								</div>
+								<div class="topic-book">
+								<p class="book">Buku Pintar</p>
+								</div>
+								<div class="book-prize">
+								<p>50
+								<img src="../Asset/images/coin.png" width="14%;" class="koin"></p>
+								</div>
+								<div class="book-buy">
+								<button class="btn btn-primary buton">Buy</button>
+								</div>
+							</div>
+							<div class="box jarak text-center">
+								<div class="image-box">
+								<img src="../Asset/images/fantasy10.jpg" class="gambar">
+								</div>
+								<div class="topic-book">
+								<p class="book">Buku Pintar</p>
+								</div>
+								<div class="book-prize">
+								<p>50
+								<img src="../Asset/images/coin.png" width="14%;" class="koin"></p>
+								</div>
+								<div class="book-buy">
+								<button class="btn btn-primary buton">Buy</button>
+								</div>
+							</div>
+							<div class="box jarak text-center">
+								<div class="image-box">
+								<img src="../Asset/images/fantasy10.jpg" class="gambar">
+								</div>
+								<div class="topic-book">
+								<p class="book">Buku Pintar</p>
+								</div>
+								<div class="book-prize">
+								<p>50
+								<img src="../Asset/images/coin.png" width="14%;" class="koin"></p>
+								</div>
+								<div class="book-buy">
+								<button class="btn btn-primary buton">Buy</button>
+								</div>
 							</div>
 						</div>
 					</div>
