4e165ab6475faa2748b2895ca04ef51b11b3faa0
David Leonardo <davidleonardo621@gmail.com>
2019-03-27 09:50:20 +0700

    .

---
+++ WashMe/view/home.php
@@ -0,0 +1,166 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"></script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>
+	<link rel="stylesheet" type="text/css" href="../asset/css/home.css">
+	<title></title>
+</head>
+<body>
+<form>
+	<div class="container-fluid-screen">
+		<div class="row">
+			<div class="col-sm-2"><img src="../asset/images/logo.png" class="logo"></div>
+			<div class="col-sm-6 border flex">
+				<div><img src="../asset/images/search.png" class="search"></div>
+					<div class="text1">Search .. </div>
+			</div>
+			<div class="col-sm-4">
+				<div class="row">
+					<div class="col-sm-3"><img src="../asset/images/coin5.png" class="coin"></div>
+					<div class="col-sm-9 text">88 coins</div>
+				</div>
+			</div>
+		</div>
+		<hr class="hr1">
+		<div class="row">
+			<div class="col-sm-1"></div>
+			<div class="col-sm-10 flex">
+				<div class="border-mid1">
+					<img src="../asset/images/washing.png" class="border-mid">
+					<div class="text-mid">Wash & Fold</div>
+				</div>
+				<div class="border-mid1">
+					<img src="../asset/images/ironing.png" class="border-mid">
+					<div class="text-mid">Wash & Iron</div>
+				</div>
+				<div class="border-mid1">
+					<img src="../asset/images/clothes.png" class="border-mid">
+					<div class="text-mid">Dry Clean</div>
+				</div>
+			</div>
+			<div class="col-sm-1"></div>
+		</div>
+		</div>
+		<div class="row background" style="margin-top: 20px;">
+			<div class="col-sm-12"></div>
+		</div>
+		<div class="row">
+			<div class="col-sm-12"><img src="../asset/images/slideshow/laundry.png" class="slide"></div>
+		</div>
+		<div class="row background">
+			<div class="col-sm-12"></div>
+		</div>
+		<div class="border-mid2">
+			<div class="row">
+				<div class="col-sm-9 text-mid1">Recommended Laundries</div>
+				<div class="col-sm-3 text-mid2">See All</div>
+			</div>
+			<div class="row">
+				<div class="col-sm-12 flex">
+					<div class="box">
+						<img src="../asset/images/laundry-ateng.jpg" class="img">
+						<div class="text">Mega, Jln. Budi Karya N...</div>
+					</div>
+					<div class="box">
+						<img src="../asset/images/mboklaundry.png" class="img">
+						<div class="text">Mbok Laundry Paris 2, Jl...</div>
+					</div>
+					<div class="box">
+						<img src="../asset/images/annisalaundry.png" class="img">
+						<div class="text">Annisa Laundry Bansir L...</div>
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="row background">
+			<div class="col-sm-12"></div>
+		</div>
+		<div class="border-mid2">
+			<div class="row">
+				<div class="col-sm-9 text-mid1">Shop</div>
+				<div class="col-sm-3 text-mid2">See All</div>
+			</div>
+			<div class="row">
+				<div class="col-sm-12 flex">
+					<div class="box2">
+						<img src="../asset/images/setrika1.png" class="img">
+						<div class="text">Setrika Uap Listrik Silver Star ES300...</div>
+						<div class="harga">Rp 515.000</div>
+						<div class="flex">
+							<img src="../asset/images/star1.png" class="star">
+							<img src="../asset/images/star1.png" class="star">
+							<img src="../asset/images/star1.png" class="star">
+							<img src="../asset/images/star1.png" class="star">
+							<img src="../asset/images/star1.png" class="star">
+							<div class="text">(3)</div>
+						</div>
+					</div>
+					<div class="box2">
+						<img src="../asset/images/shop1.jpg" class="img">
+						<div class="text">Stand Hanger / Gantungan tiang...</div>
+						<div class="harga">Rp 70.000</div>
+						<div class="flex">
+							<img src="../asset/images/star1.png" class="star">
+							<img src="../asset/images/star1.png" class="star">
+							<img src="../asset/images/star1.png" class="star">
+							<img src="../asset/images/star1.png" class="star">
+							<img src="../asset/images/star1.png" class="star">
+							<div class="text">(89)</div>
+						</div>
+					</div>
+					<div class="box2">
+						<img src="../asset/images/shop2.jpg" class="img">
+						<div class="text">Jemuran Dinding Fortuna (JD-150)</div>
+						<div class="harga">Rp 340.000</div>
+						<div class="flex">
+							<img src="../asset/images/star1.png" class="star">
+							<img src="../asset/images/star1.png" class="star">
+							<img src="../asset/images/star1.png" class="star">
+							<img src="../asset/images/star1.png" class="star">
+							<img src="../asset/images/star1.png" class="star">
+							<div class="text">(156)</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="row background">
+			<div class="col-sm-12"></div>
+		</div>
+		<div class="row box3">
+			<div class="col-sm-7">
+				<div class="text1">Have a laundry? Open your online branch in WashMe</div>
+				<div class="text2">Easy to use and trusted. FREE!</div>
+				<div class="flex">
+					<button class="button">Open your Laundry</button>
+					<div class="text3"><u>Learn more</u></div>
+				</div>
+			</div>
+			<div class="col-sm-5"><img src="../asset/images/laundry3.png" class="img"></div>
+		</div>
+		<hr class="hr2">
+		<div class="flex footer">
+			<div class="box4">
+				<img src="../asset/images/home.png" class="img2">
+				<div class="text">Home</div>
+			</div>
+			<div class="box4">
+				<img src="../asset/images/shopping-list2.png" class="img2">
+				<div class="text">My Orders</div>
+			</div>
+			<div class="box4">
+				<img src="../asset/images/shopping-cart2.png" class="img2">
+				<div class="text">WashMe Shop</div>
+			</div>
+			<div class="box4">
+				<img src="../asset/images/profile2.png" class="img2">
+				<div class="text">My Account</div>
+			</div>
+		</div>	
+	</div>
+</form>
+</body>
+</html>
---
+++ WashMe/view/myorders.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"></script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>
+	<link rel="stylesheet" type="text/css" href="../asset/css/myorders.css">
+	<title></title>
+</head>
+<body>
+<div class="container-fluid-screen">
+	<div class="row">
+		<div class="col-sm-12 header" >My Orders</div>
+	</div>
+	<hr class="hr1">
+	<div class="row background">
+		<div class="col-sm-12"></div>
+	</div>
+	<div class="row box">
+		<div class="col-sm-6 text1">Progress Details</div>
+		<div class="col-sm-6 text2">Order WMO-1234567</div>
+	</div>
+</div>
+</body>
+</html>
