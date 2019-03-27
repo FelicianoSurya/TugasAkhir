8c2437af848aa08931eefc4c316aff5fe68a6c10
Zauhiung <emmanuel.karyaten@gmail.com>
2019-03-27 12:12:01 +0700

    .

--- WashMe/view/home.php
+++ WashMe/view/home.php
@@ -1,12 +1,13 @@
 <!DOCTYPE html>
 <html>
 <head>
+	<title>WashMe Mobile</title>
+	<link rel="icon" href="../asset/images/logo.png">
+	<link rel="stylesheet" type="text/css" href="../asset/css/home.css">
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"></script>
 	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>
-	<link rel="stylesheet" type="text/css" href="../asset/css/home.css">
-	<title></title>
 </head>
 <body>
 <form>
@@ -143,7 +144,7 @@
 		</div>
 		<hr class="hr2">
 		<div class="flex footer">
-			<a href="#">
+			<a href="home.php">
 				<div class="box4">
 					<img src="../asset/images/home.png" class="img2">
 					<div class="text">Home</div>
@@ -167,7 +168,7 @@
 					<div class="text">My Account</div>
 				</div>
 			</a>
-		</div>	
+		</div>
 	</div>
 </form>
 </body>
--- WashMe/view/myaccount.php
+++ WashMe/view/myaccount.php
@@ -6,7 +6,7 @@
 <head>
 	<title>WashMe Mobile</title>
 	<link rel="icon" href="../asset/images/logo.png">
-	<link rel="stylesheet" type="text/css" href="../asset/css/myaccount.css">
+	<link rel="stylesheet" type="text/css" href="../asset/css/myaccount1.css">
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
@@ -35,7 +35,91 @@
 				<button type="button" id="profile">Edit Profile</button>
 			</div>
 		</div>
+		<hr class="hr1">
+		<div class="content-in1">
+			<div class="user">
+				<p>Click below to connect your WashMe account with<br>Facebook</p>
+			</div>
+			<div class="edit">
+				<button type="button" id="profile"><span><img src="../asset/images/facebook-logo.png"></span><p>Connect with Facebook</p></button>
+			</div>
+		</div>
+		<hr class="hr1">
+		<a href="#">
+			<div class="content-in2">
+				<div class="promo">
+					<p class="promo-in">Enter Promo Code<span><img src="../asset/images/right-arrow.png"></span></p>
+				</div>
+			</div>
+		</a>
+		<hr class="hr1">
+		<a href="#">
+			<div class="content-in2">
+				<div class="promo">
+					<p class="promo-in1">My Voucher<span><img src="../asset/images/right-arrow.png"></span></p>
+				</div>
+			</div>
+		</a>
+		<hr class="hr1">
+		<a href="#">
+			<div class="content-in2">
+				<div class="promo">
+					<p class="promo-in2">Language<span><img src="../asset/images/right-arrow.png"></span></p>
+				</div>
+			</div>
+		</a>
+		<hr class="hr1">
+		<a href="#">
+			<div class="content-in2">
+				<div class="promo">
+					<p class="promo-in3">Help<span><img src="../asset/images/right-arrow.png"></span></p>
+				</div>
+			</div>
+		</a>
+		<hr class="hr1">
+		<a href="#">
+			<div class="content-in2">
+				<div class="promo">
+					<p class="promo-in4">Frequently Ask Questions<span><img src="../asset/images/right-arrow.png"></span></p>
+				</div>
+			</div>
+		</a>
+		<hr class="hr1">
+		<a href="#">
+			<div class="content-in2">
+				<div class="promo">
+					<p class="promo-in5">Rate WashMe App<span>3.20</span></p>
+				</div>
+			</div>
+		</a>
 	</div>
+	<hr class="hr2">
+	<div class="flex footer">
+		<a href="home.php">
+			<div class="box4">
+				<img src="../asset/images/home2.png" class="img2">
+				<div class="text">Home</div>
+			</div>
+		</a>
+		<a href="#">
+			<div class="box4">
+				<img src="../asset/images/shopping-list2.png" class="img2">
+				<div class="text">My Orders</div>
+			</div>
+		</a>
+		<a href="#">
+			<div class="box4">
+				<img src="../asset/images/shopping-cart2.png" class="img2">
+				<div class="text">WashMe Shop</div>
+			</div>
+		</a>
+		<a href="myaccount.php">
+			<div class="box4">
+				<img src="../asset/images/profile.png" class="img2">
+				<div class="text">Account</div>
+			</div>
+		</a>
+	</div>	
 </div>
 </body>
 </html>
