4d35d97c643619a5a79f791b12aa35ff498891e4
David Leonardo <davidleonardo621@gmail.com>
2019-03-27 12:27:23 +0700

    .

--- WashMe/view/myorders.php
+++ WashMe/view/myorders.php
@@ -21,6 +21,62 @@
 		<div class="col-sm-6 text1">Progress Details</div>
 		<div class="col-sm-6 text2">Order WMO-1234567</div>
 	</div>
+	<div class="row box2">
+		<div class="col-sm-1"></div>
+		<div class="col-sm-10">
+			<div class="row">
+				<div class="col-sm-2"><img src="../asset/images/tag2.png" class="img"></div>
+				<div class="col-sm-8 text1">Payment</div>
+				<div class="col-sm-2"><img src="../asset/images/check.png" class="img"></div>
+			</div>
+			<div class="row top">
+				<div class="col-sm-2"><img src="../asset/images/delivery-truck.png" class="img2"></div>
+				<div class="col-sm-8 text">Launderer Pick Up Clothes</div>
+				<div class="col-sm-2"><img src="../asset/images/loading.png" class="img"></div>
+			</div>
+			<div class="row top">
+				<div class="col-sm-2"><img src="../asset/images/laundry-basket.png" class="img2"></div>
+				<div class="col-sm-8 text">Collect your clothes</div>
+				<div class="col-sm-2"><img src="../asset/images/clock.png" class="img"></div>
+			</div>
+			<div class="row top">
+				<div class="col-sm-2"><img src="../asset/images/washing-clothes.png" class="img2"></div>
+				<div class="col-sm-8 text">Job Accepted</div>
+				<div class="col-sm-2"><img src="../asset/images/clock.png" class="img"></div>
+			</div>
+			<div class="row top">
+				<div class="col-sm-2"><img src="../asset/images/washing-machine2.png" class="img2"></div>
+				<div class="col-sm-8 text">Cleaning Clothes</div>
+				<div class="col-sm-2"><img src="../asset/images/clock.png" class="img"></div>
+			</div>
+			<div class="row top">
+				<div class="col-sm-2"><img src="../asset/images/suit.png" class="img2"></div>
+				<div class="col-sm-8 text">Delivering Clothes</div>
+				<div class="col-sm-2"><img src="../asset/images/clock.png" class="img"></div>
+			</div>
+			<button class="button">Received Clothes</button>
+		</div>
+		<div class="col-sm-1"></div>
+	</div>
+	<hr class="hr2">
+	<div class="flex footer">
+		<div class="box4">
+			<img src="../asset/images/home.png" class="img2">
+			<div class="text">Home</div>
+		</div>
+		<div class="box4">
+			<img src="../asset/images/shopping-list2.png" class="img2">
+			<div class="text">My Orders</div>
+		</div>
+		<div class="box4">
+			<img src="../asset/images/shopping-cart2.png" class="img2">
+			<div class="text">WashMe Shop</div>
+		</div>
+		<div class="box4">
+			<img src="../asset/images/profile2.png" class="img2">
+			<div class="text">My Account</div>
+		</div>
+	</div>
 </div>
 </body>
 </html>
