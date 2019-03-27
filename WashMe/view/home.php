8788e135f74499ecc6bfdaaac44516306b99d2c0
David Leonardo <davidleonardo621@gmail.com>
2019-03-27 19:54:49 +0700

    .

--- WashMe/view/home.php
+++ WashMe/view/home.php
@@ -3,7 +3,7 @@
 <head>
 	<title>WashMe Mobile</title>
 	<link rel="icon" href="../asset/images/logo.png">
-	<link rel="stylesheet" type="text/css" href="../asset/css/home.css">
+	<link rel="stylesheet" type="text/css" href="../asset/css/home1.css">
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"></script>
---
+++ WashMe/view/washme-order.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"></script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>
+	<link rel="stylesheet" type="text/css" href="../asset/css/washme-order1.css">
+	<title></title>
+</head>
+<body>
+<form>
+	<div class="container-fluid-screen">
+		<div class="background">
+			<div class="back"><img src="../asset/images/left-arrow.png" class="img"></div>
+		</div>
+		<div class="padding flex">
+			<div class="text">Mega Laundry, Jln Budi Karya No.15</div>
+			<div class="border-mid"><img src="../asset/images/map.png" class="img"></div>
+		</div>
+		<div class="padding jarak">0.94 Km</div>
+		<hr class="hr">
+		<div class="padding flex box1">
+			<div><img src="../asset/images/clock3.png" class="img"></div>
+			<div class="text1">OPEN</div>
+			<div class="text2">until 19:00 today</div>
+		</div>
+		<div class="background1">
+			<button class="button">Fill Order Format</button>
+		</div>
+	</div>
+</form>
+</body>
+</html>
