efaac8e38af61395ba4b5a5d12eed204f7fd4123
Zauhiung <emmanuel.karyaten@gmail.com>
2019-03-29 01:11:53 +0700

    .

---
+++ WashMe/view/chat.php
@@ -0,0 +1,98 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>WashMe Mobile</title>
+	<link rel="icon" href="../asset/images/logo.png">
+	<link rel="stylesheet" type="text/css" href="../asset/css/chat.css">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+</head>
+<body>
+<div class="container-fluid-fullscreen">
+	<div class="header">
+		<div class="topic-top">
+			<header>
+				<a href="home.php">
+					<div class="logo">
+						<img src="../asset/images/left-arrow.png" alt="image">
+					</div>
+				</a>
+				<div class="list">
+					<div class="geser">
+						<img src="../asset/images/courier.png" alt="image">
+					</div>
+					<div class="nama">
+						<h5 class="h5">Andi</h5>
+						<p>KB 1234 WQ</p>
+					</div>
+					<div class="call">
+						<img src="../asset/images/call-answer.png" alt="images">
+					</div>
+				</div>
+			</header>
+		</div>
+	</div>
+	<div class="content">
+		<div class="content-in">
+			<div class="kotak-chat">
+				<div class="chat">
+					<p>Otw</p>
+				</div>
+				<div class="waktu">
+					<p>16:08</p>
+				</div>
+			</div>
+		</div>
+		<div class="content-in1">
+			<div class="kotak-chat">
+				<div class="chat">
+					<p>Ok</p>
+				</div>
+				<div class="waktu">
+					<p>16:09<span><img src="../asset/images/double-tick-indicator.png"></span></p>
+				</div>
+			</div>
+		</div>
+		<div class="content-in2">
+			<div class="kotak-chat">
+				<div class="chat">
+					<p>Saya sudah sampai</p>
+				</div>
+				<div class="waktu">
+					<p>16:18</p>
+				</div>
+			</div>
+		</div>
+		<div class="content-in3">
+			<div class="kotak-chat">
+				<div class="chat">
+					<p>Ok</p>
+				</div>
+				<div class="waktu">
+					<p>16:20<span><img src="../asset/images/check-symbol.png"></span></p>
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="footer">
+		<div class="topic-top">
+			<footer>
+				<div class="logo">
+					<img src="../asset/images/plus.png" alt="image">
+				</div>
+				<div class="list">
+					<div class="kotak-search">
+						<input type="text" name="search" id="search" placeholder="Notes to driver" class="search">
+					</div>
+					<div class="call">
+						<img src="../asset/images/send1.png" alt="images">
+					</div>
+				</div>
+			</footer>
+		</div>
+	</div>
+</div>
+</body>
+</html>
