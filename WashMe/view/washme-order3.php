ca7fe1122919a63cd8130fb515f58f123d6b6c9e
David Leonardo <davidleonardo621@gmail.com>
2019-03-28 20:04:00 +0700

    .

---
+++ WashMe/view/washme-order3.php
@@ -0,0 +1,74 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"></script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>
+	<link rel="stylesheet" type="text/css" href="../asset/css/washme-order3.css">
+	<title></title>
+</head>
+<body>
+<div class="container-fluid-screen">
+	<div class="flex border-top">
+		<div><img src="../asset/images/left-arrow2.png" class="img"></div>
+		<div class="text">Laundry Item</div>
+	</div>
+	<div class="flex padding">
+		<div class="text">Basic Laundry (Kg)</div>
+		<div class="text border-mid1">Dry Cleaning (Pcs)</div>
+	</div>
+	<div class="flex">
+		<hr class="hr1">
+		<hr class="hr2">
+	</div>
+	<div class="box">
+		<div class="box">
+			<div class="flex">
+				<div class="box-in1">
+					<div class="text1">Wash & Fold</div>
+					<div class="text2">Rp 5.000 / Kg</div>
+				</div>
+				<div class="flex box-in1 jarak">
+					<div class="min"><img src="../asset/images/minus.png" class="img"></div>
+					<div class="jarak1">
+						<div class="text1">3</div>
+						<div class="text2">(Kg)</div>
+					</div>
+					<div class="max"><img src="../asset/images/plus.png" class="img"></div>
+				</div>
+			</div>
+		</div>
+		<hr class="hr3">
+		<div class="box">
+			<div class="flex">
+				<div class="box-in1">
+					<div class="text1">Wash & Iron</div>
+					<div class="text2">Rp 8.000 / Kg</div>
+				</div>
+				<div class="flex box-in1 jarak">
+					<div class="min"><img src="../asset/images/minus.png" class="img"></div>
+					<div class="jarak1">
+						<div class="text1">0</div>
+						<div class="text2">(Kg)</div>
+					</div>
+					<div class="max"><img src="../asset/images/plus.png" class="img"></div>
+				</div>
+			</div>
+		</div>
+		<hr class="hr3">
+		<div class="flex box2">
+			<div><img src="../asset/images/weight.png" class="img"></div>
+			<div class="box-in">
+				<div class="text1">Not sure how much your laundry weights?</div>
+				<div class="text2">Don't worry, our picker will always recalculate the weight during pick up.</div>
+			</div>
+		</div>
+		<div class="note">P.S : Please seperate the clothes according the services.</div>
+		<div class="next">
+			<img src="../asset/images/right-arrow2.png">
+		</div>
+	</div>
+</div>
+</body>
+</html>
