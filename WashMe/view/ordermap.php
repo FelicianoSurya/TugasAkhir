e177d9eb803df232108bee86f84c1103521d21fc
Zauhiung <emmanuel.karyaten@gmail.com>
2019-03-29 16:05:07 +0700

    .

---
+++ WashMe/view/ordermap.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>WashMe Mobile</title>
+	<link rel="icon" href="../asset/images/logo.png">
+	<link rel="stylesheet" type="text/css" href="../asset/css/ordermap.css">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+</head>
+<body>
+<div class="container-fluid-fullscreen">
+	<div class="header">
+		<div class="topic-top">
+			<header>
+				<div class="logo">
+					<img src="../asset/images/map1.jpg" alt="image">
+				</div>
+			</header>
+		</div>
+	</div>
+	<div class="content">
+		<div class="content-in">
+			<div class="content-top">
+				<img src="../asset/images/line.png">
+			</div>
+			<div class="content-mid">
+				<div class="nama">
+					<h3>Mega Laundry</h3>
+				</div>
+				<div class="alamat">
+					<p>Jln. Budi Karya No.15, Pontianak Selatan, Pontianak</p>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+</body>
+</html>
