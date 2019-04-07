6b1beb9f234c6f31124e480f6640a2296493988f
FelicianoSurya <suryamarcello@gmail.com>
2019-04-07 21:06:46 +0700

    ,

---
+++ WashMe/web/js/admin/admin.js
@@ -0,0 +1,3 @@
+function del_data(id){
+	location.href = "../../php/admin/admin_user_php.php?submit=Delete&id="+id;
+}
--- WashMe/web/view/admin/admin-request.php
+++ WashMe/web/view/admin/admin-request.php
@@ -2,7 +2,7 @@
 <head>
 	<title>Wash Me</title>
 	<link rel="icon" href="../../asset/images/logo.png">
-	<link rel="stylesheet" type="text/css" href="../../asset/css/admin-user.css">
+	<link rel="stylesheet" type="text/css" href="../../asset/css/admin-request.css">
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"></script>
@@ -83,47 +83,8 @@
 			</div>
 		</div>
 		<div class="right">
-			<div class="body-right">
-				<table border="4">
-					<tr>
-						<th>Id</th>
-						<th>Name</th>
-						<th>Email</th>
-						<th>Action</th>
-					</tr>
-					<?php 
-					include "../../php/connection.php";
-					$sql = 'select * from users';
-					$query = mysqli_query($conn,$sql);
-					$num = mysqli_num_rows($query);
 
-					for($x=1;$x<=$num;$x++){
-						$re = mysqli_fetch_array($query);
-						if($re['status'] == 'user'){
-						$id = $re['id'];
-						$username = $re['username'];
-						$email = $re['email'];
-					 ?>
-					
-					<tr>
-						<td><?php echo $id ?></td>
-						<td><?php echo $username ?></td>
-						<td><?php echo $email ?></td>
-						<td>
-							<input type='submit'class="btn-sm btn-danger" id="delete" name="delete" value="Delete" onclick="del_data(<?php echo $id ?>)">
-						</td>
-					</tr>
-					<?php }  }?>
-					<tr>
-						<th>Id</th>
-						<th>Name</th>
-						<th>Email</th>
-						<th>Action</th>
-					</tr>
-				</table>
-			</div>
 		</div>
 	</div>
 </body>
 </html>
-<script src="../../js/admin/admin.js"></script>
