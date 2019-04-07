fc341fff151910679d5efff30f4b6170dad30cc0
David Leonardo <davidleonardo621@gmail.com>
2019-04-07 22:30:57 +0700

    .

--- WashMe/php/register_success.php
+++ WashMe/php/register_success.php
@@ -6,13 +6,14 @@ $username = $_POST['username'];
 $email = $_POST['email'];
 $password = $_POST['password'];
 $confirmation_password = $_POST['confirmation'];
+$nohp = $_POST['nohp'];
 
 $submit = $_POST['register'];
 
 if($submit == 'Register'){
-	if($username !="" && $email !="" && $password !=""){
+	if($username !="" && $email !="" && $password !="" && $nohp !=""){
 		if($password == $confirmation_password){
-			$sql = "insert into users (username,password,email,status) values ('$username','$password','$email','user')";
+			$sql = "insert into users (username,password,email,nohp,status) values ('$username','$password','$email','$nohp','user')";
 			$query = mysqli_query($conn,$sql);
 			header('location:../view/index.php?pesan=berhasil');	
 		}
