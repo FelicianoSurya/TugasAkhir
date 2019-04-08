6176c69ee5759066b764f91bc7f100bd5d559309
David Leonardo <davidleonardo621@gmail.com>
2019-04-08 22:07:10 +0700

    .

--- WashMe/web/view/washmeshop.php
+++ WashMe/web/view/washmeshop.php
@@ -2,16 +2,16 @@
 <head>
 	<title>Wash Me</title>
 	<link rel="icon" href="../asset/images/logo.png">
-	<link rel="stylesheet" type="text/css" href="../asset/css/washmeshop1.css">
+	<link rel="stylesheet" type="text/css" href="../asset/css/washmeshop2.css">
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"></script>
 	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>
 </head>
 <body>
-	<?p
+	<?php
 	session_start();
-	 ?>-->
+	 ?>
 	 <div class="big-body">
 		<div class="body">
 			<div class="left">
