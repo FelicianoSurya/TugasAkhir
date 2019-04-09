8a2e4ce1c1d6503e22b50850f27933d5a7901b20
David Leonardo <davidleonardo621@gmail.com>
2019-04-09 21:54:03 +0700

    .

---
+++ WashMe/web/view/home1-1.php
@@ -0,0 +1,91 @@
+ <head>
+	<title>Wash Me</title>
+	<link rel="icon" href="../asset/images/logo.png">
+	<link rel="stylesheet" type="text/css" href="../asset/css/home1-.css">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"></script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>
+</head>
+<body>
+<div class="background">
+	<div class="flex">
+		<div class="box-top1">
+			<img src="../asset/images/annisalaundry.png" class="annisa">
+			<div class="flex">
+				<img src="../asset/images/annisalaundry.png" class="annisa1" style="margin-top: 10px;">
+				<img src="../asset/images/annisalaundry.png" class="annisa1" style="margin: 10px 0px 0px 15px;">
+				<img src="../asset/images/annisalaundry.png" class="annisa1" style="margin: 10px 0px 0px 15px;">
+			</div>
+		</div>
+		<div class="box-top2">
+			<div class="text1">Annisa Laundry</div>
+			<div class="text2">Jln. Gajah Mada 16 No.20</div>
+			<div class="text3">Closed</div>
+			<div class="text4">08.00 - 21.00</div>
+			<div class="text5">Rp 6000 / Kg</div>
+			<div class="flex">
+				<button class="button kanan">Pick Up</button>
+				<button class="button">Delivery</button>
+			</div>
+		</div>
+		<img src="../asset/images/maps.jpg" class="map">
+	</div>
+	<div class="flex">
+		<div class="border1">
+			<div class="flex jarak">
+				<img src="../asset/images/boy.png" class="boy">
+				<div class="box">
+					<div>Laundry Owner :</div>
+					<div>Feliciano Surya</div>
+				</div>
+				<div class="pembatas"></div>
+				<div class="box2">
+					<div>Share :</div>
+					<div class="flex">
+						<img src="../asset/images/facebook.png" class="sosmed">
+						<img src="../asset/images/instagram.png" class="sosmed">
+						<img src="../asset/images/google.png" class="sosmed">
+					</div>
+				</div>
+			</div>
+			<hr class="hr">
+			<div class="box3">
+				<div class="kotak">
+					<div>User</div>
+					<div>100</div>
+				</div>
+				<div class="flex kotak">
+					<div class="pembatas"></div>
+					<div class="jarak1">
+						<div>Order Success</div>
+						<div>90%</div>
+					</div>
+					<div class="pembatas"></div>
+				</div>
+				<div class="flex kotak">
+					<div>
+						<div>Rating</div>
+						<div>4.8</div>
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="border2">
+			<div class="word">Available :</div>
+			<div class="kotak1">
+				<img src="../asset/images/washing.png" class="img1">
+				<div class="wash1">Wash & Fold</div>
+			</div>
+			<div class="kotak1">
+				<img src="../asset/images/ironing.png" class="img1">
+				<div class="wash1">Wash & Iron</div>
+			</div>
+			<div class="kotak1">
+				<img src="../asset/images/clothes.png" class="img1">
+				<div class="wash1">Dry Clean</div>
+			</div>
+		</div>
+	</div>
+</div>
+</body>
