39b0438c662c101517197eb702813825bd2514bc
FelicianoSurya <suryamarcello@gmail.com>
2019-04-09 11:44:29 +0700

    .

--- WashMe/web/view/home.php
+++ WashMe/web/view/home.php
@@ -2,7 +2,7 @@
 <head>
 	<title>Wash Me</title>
 	<link rel="icon" href="../asset/images/logo.png">
-	<link rel="stylesheet" type="text/css" href="../asset/css/home.css">
+	<link rel="stylesheet" type="text/css" href="../asset/css/home1.css">
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"></script>
@@ -81,7 +81,6 @@
 							$close = $re['time_close'];
 							$harga = $re['harga'];
 							$kota = $re['kota'];
-							$image = $re['image'];
 
 						?>
 						<div class="box-place">
