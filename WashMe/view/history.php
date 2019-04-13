82520d19ab760ade83b4a2b4508f7134160c8c53
David Leonardo <davidleonardo621@gmail.com>
2019-04-13 21:54:42 +0700

    .

---
+++ WashMe/view/history.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>WashMe Mobile</title>
+	<link rel="icon" href="../asset/images/logo.png">
+	<link rel="stylesheet" type="text/css" href="../asset/css/history.css">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"></script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>
+</head>
+<body>
+<div class="header flex">
+	<img src="../asset/images/left-arrow2.png" class="arrow">
+	<div class="title">History</div>
+</div>
+<div class="body flex jarak">
+	<img src="../asset/images/delivery-truck.png" class="img">
+	<div>
+		<div class="text">Delivered</div>
+		<div class="text1">Mega Laundry - Jln  Budi Karya No. 15</div>
+		<div class="text2">21 March - 23 March</div>
+	</div>
+</div>
+<div class="body flex">
+	<img src="../asset/images/delivery-truck.png" class="img">
+	<div>
+		<div class="text3">Order Cancelled</div>
+		<div class="text1">Mega Laundry - Jln  Budi Karya No. 15</div>
+		<div class="text2">21 March - 23 March</div>
+	</div>
+</div>
+<div class="body flex">
+	<img src="../asset/images/delivery-truck.png" class="img">
+	<div>
+		<div class="text">Delivered</div>
+		<div class="text1">Mega Laundry - Jln  Budi Karya No. 15</div>
+		<div class="text2">21 March - 23 March</div>
+	</div>
+</div>
+</body>
+</html>
