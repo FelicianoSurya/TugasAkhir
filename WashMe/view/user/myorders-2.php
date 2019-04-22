19c722b610597503250169f69b6ccbebe003c4f7
David Leonardo <davidleonardo621@gmail.com>
2019-04-22 22:09:02 +0700

    .

---
+++ WashMe/view/user/myorders-2.php
@@ -0,0 +1,88 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"></script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>
+	<link rel="stylesheet" type="text/css" href="../asset/css/myorders-.css">
+	<title></title>
+</head>
+<body>
+<form>
+	<div class="container-fluid-fullscreen">
+		<div class="header">My Orders</div>
+		<hr>
+		<div class="body">
+			<div class="flex2">
+				<div class="box">
+				<div class="flex">
+					<div class="top1">Pick Up</div>
+					<div class="top2">Order WMO-1234567</div>
+				</div>
+				<div class="flex1 top">
+					<img src="../asset/images/destination.png" class="img">
+					<div class="left">
+						<div class="text">Jln. Veteran gg. Syukur 6</div>
+						<hr class="top">
+						<div class="text">Mega Laundry</div>
+						<div class="text">Jln. Budi Karya No. 15</div>
+					</div>
+				</div>
+				<img src="../asset/images/maps.jpg" class="img1">
+				<div class="text1">double click map to zoom</div>
+				<div class="profile">
+					<div class="center">
+						<img src="../asset/images/driver.png" class="img">
+						<div class="text2">Andi</div>
+						<div class="text2">08123456789</div>
+					</div>
+				</div>
+				<div class="flex">
+					<button class="button">
+						<img src="../asset/images/mail-send.png" class="img1">
+						<p>Send Message</p>
+					</button>
+					<button class="button">
+						<img src="../asset/images/telephone.png" class="img2">
+						<p>Call</p>
+					</button>
+				</div>
+			</div>
+		</div>
+		<div class="flex2">
+			<button class="button2">
+				<img src="../asset/images/close-browser.png" class="img">
+				<p>Cancel Order</p>
+			</button>	
+		</div>
+		<div class="footer">
+			<a href="home.php">
+				<div class="box4">
+					<img src="../asset/images/home.png" class="img2">
+					<div class="text">Home</div>
+				</div>
+			</a>
+			<a href="#">
+				<div class="box4">
+					<img src="../asset/images/shopping-list2.png" class="img2">
+					<div class="text1">Orders</div>
+				</div>
+			</a>
+			<a href="washmeshop.php">
+				<div class="box4">
+					<img src="../asset/images/shopping-cart2.png" class="img2">
+					<div class="text1">WashMe Shop</div>
+				</div>
+			</a>
+			<a href="myaccount.php">
+				<div class="box4">
+					<img src="../asset/images/profile2.png" class="img2">
+					<div class="text1">Account</div>
+				</div>
+			</a>
+		</div>
+	</div>
+</form>
+</body>
+</html>
