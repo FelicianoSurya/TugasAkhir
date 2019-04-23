d60bbf548a991ab4328f587d92ac48e65a14a6c1
David Leonardo <davidleonardo621@gmail.com>
2019-04-23 20:54:12 +0700

    .

---
+++ WashMe/view/admin/washme-laundry.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>WashMe Mobile</title>
+	<link rel="icon" href="../../asset/images/logo.png">
+	<link rel="stylesheet" type="text/css" href="../../asset/css/admin/washme-laundry1.css">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+</head>
+<body>
+<form>
+	<div class="container-fluid-screen">
+		<div class="header">
+			<img src="../../asset/images/logo.png" class="logo">
+			<div class="flex"><p>Admin</p></div>
+		</div>
+		<hr class="hr">
+		<div class="content">
+			<div class="box">
+				<div class="text">Laundry's Account</div>
+				<img src="../../asset/images/clothes2.png">
+			</div>
+			<div class="box">
+				<div class="text">Laundry's Report</div>
+				<img src="../../asset/images/diagram.png">
+			</div>
+		</div>
+		<div class="footer">
+			<a href="home.php">
+				<div class="box4">
+					<img src="../../asset/images/profile2.png" class="img2">
+					<div class="text1">User</div>
+				</div>
+			</a>
+			<a href="#">
+				<div class="box4">
+					<img src="../../asset/images/washing-machine4.png" class="img2">
+					<div class="text">Laundry</div>
+				</div>
+			</a>
+			<a href="washmeshop.php">
+				<div class="box4">
+					<img src="../../asset/images/shopping-list2.png" class="img2">
+					<div class="text1">Request</div>
+				</div>
+			</a>
+			<a href="myaccount.php">
+				<div class="box4">
+					<img src="../../asset/images/settings3.png" class="img2">
+					<div class="text1">Setting</div>
+				</div>
+			</a>
+		</div>
+	</div>
+</form>
+</body>
+</html>
