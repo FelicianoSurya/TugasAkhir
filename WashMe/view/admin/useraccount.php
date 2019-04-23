697669d9ec8e10b7cd93c6db360835ea4c74e959
David Leonardo <davidleonardo621@gmail.com>
2019-04-23 22:05:08 +0700

    .

---
+++ WashMe/view/admin/useraccount.php
@@ -0,0 +1,123 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>WashMe Mobile</title>
+	<link rel="icon" href="../../asset/images/logo.png">
+	<link rel="stylesheet" type="text/css" href="../../asset/css/admin/useraccount.css">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+</head>
+<body>
+<form>
+	<div class="container-fluid-screen">
+		<div class="header">
+			<img src="../../asset/images/logo.png" class="logo">
+			<div class="atas">
+				<p>Admin</p>
+			</div>
+		</div>
+		<hr class="hr">
+		<div class="content">
+			<div class="text1">User's Account</div>
+			<div class="input">
+				<img src="../../asset/images/search.png">
+				<input type="text" name="search" id="search" placeholder="Search...">
+			</div>
+			<div class="text1">Select Location</div>
+			<select class="select">
+				<option>Pontianak, Kalimantan barat</option>
+				<option>#</option>
+			</select>
+			<div class="box top">
+				<div class="flex">
+					<div class="text1">ID</div>
+					<div class="text2">:</div>
+					<div class="text3">WM1000</div>
+				</div>
+				<div class="flex">
+					<div class="text1">Name</div>
+					<div class="text2">:</div>
+					<div class="text3">Fleren Picestelia</div>
+				</div>
+				<div class="flex">
+					<div class="text1">Phone</div>
+					<div class="text2">:</div>
+					<div class="text3">08992959698</div>
+				</div>
+				<div class="flex">
+					<div class="text1">Email</div>
+					<div class="text2">:</div>
+					<div class="text3">flerenpicestelia05@gmail.com</div>
+				</div>
+				<div class="flex">
+					<div class="text1">Location</div>
+					<div class="text2">:</div>
+					<div class="text3">Jln Veteran gg Syukur 1 No 50D</div>
+				</div>
+				<div class="button">
+					<button>Block</button>
+				</div>
+			</div>
+			<div class="box">
+				<div class="flex">
+					<div class="text1">ID</div>
+					<div class="text2">:</div>
+					<div class="text3">WM1000</div>
+				</div>
+				<div class="flex">
+					<div class="text1">Name</div>
+					<div class="text2">:</div>
+					<div class="text3">Feliciano Surya Marcelli</div>
+				</div>
+				<div class="flex">
+					<div class="text1">Phone</div>
+					<div class="text2">:</div>
+					<div class="text3">08992959698</div>
+				</div>
+				<div class="flex">
+					<div class="text1">Email</div>
+					<div class="text2">:</div>
+					<div class="text3">feliciano@gmail.com</div>
+				</div>
+				<div class="flex">
+					<div class="text1">Location</div>
+					<div class="text2">:</div>
+					<div class="text3">Jln Sungai Raya Dalam Kompleks Villa Gading Raya 2</div>
+				</div>
+				<div class="button">
+					<button>Block</button>
+				</div>
+			</div>
+		</div>
+		<div class="footer">
+			<a href="home.php">
+				<div class="box4">
+					<img src="../../asset/images/profile.png" class="img2">
+					<div class="text">User</div>
+				</div>
+			</a>
+			<a href="#">
+				<div class="box4">
+					<img src="../../asset/images/washing-machine5.png" class="img2">
+					<div class="text1">Laundry</div>
+				</div>
+			</a>
+			<a href="washmeshop.php">
+				<div class="box4">
+					<img src="../../asset/images/shopping-list2.png" class="img2">
+					<div class="text1">Request</div>
+				</div>
+			</a>
+			<a href="myaccount.php">
+				<div class="box4">
+					<img src="../../asset/images/settings3.png" class="img2">
+					<div class="text1">Setting</div>
+				</div>
+			</a>
+		</div>
+	</div>
+</form>
+</body>
+</html>
