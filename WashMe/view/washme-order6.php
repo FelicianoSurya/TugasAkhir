f19c6f701290f644e2dbc937657e68b007ba7245
David Leonardo <davidleonardo621@gmail.com>
2019-04-24 22:39:03 +0700

    .

---
+++ WashMe/view/washme-order6.php
@@ -0,0 +1,82 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>WashMe Mobile</title>
+	<link rel="icon" href="../asset/images/logo.png">
+	<link rel="stylesheet" type="text/css" href="../asset/css/washme-order6.css">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"></script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>
+</head>
+<body>
+<form>
+	<div class="container-fluid-screen">
+		<div class="header">
+			<img src="../asset/images/left-arrow2.png">
+			<div class="atas"><p>Confirmation</p></div>
+		</div>
+		<div class="body">
+			<div class="box">
+				<p class="text1">Date</p>
+				<p class="text2">Tuesday, 26 March 2019</p>
+			</div>
+			<div class="box">
+				<p class="text1">Time</p>
+				<p class="text2">15:00 to 16:00</p>
+			</div>
+			<div class="box">
+				<p class="text1">Place</p>
+				<p class="text2">Home</p>
+				<p class="text3">Jln Veteran gg Syukur 1 o 50D</p>
+				<hr class="hr">
+			</div>
+			<div class="box">
+				<p class="text1">Laundry</p>
+				<p class="text3">Mega Laundry - Jln Budi Karya No.15</p>
+				<hr class="hr">
+			</div>
+			<div class="box">
+				<div class="text1">Laundry Detail</div>
+				<div class="box-in">
+					<div class="text4">(3 Kg) Wash & Fold</div>
+					<div class="harga">Rp 15.000</div>
+				</div>
+				<div class="box-in">
+					<div class="text4">(2 Pcs) Dry Cleaning</div>
+					<div class="harga">Rp 25.000</div>
+				</div>
+				<div class="box-in">
+					<div class="text4">Extra</div>
+					<div class="harga">Rp 19.000</div>
+				</div>
+				<div class="box-in">
+					<div class="text4">Fee</div>
+					<div class="harga">Rp 3.000</div>
+				</div>
+				<hr class="hr">
+			</div>
+			<div class="box bawah">
+				<div class="box-in">
+					<img src="../asset/images/tag.png" class="img">
+					<div class="kiri"><u>Promo Code</u></div>
+				</div>
+			</div>
+			<hr class="hr">
+			<div class="box bawah">
+				<div class="box-in1">
+					<div class="text1">Total</div>
+					<div class="text5">Rp 59.000</div>
+				</div>
+			</div>
+			<hr class="hr">
+			<div class="center">
+				<div class="button">
+					<div>Order</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</form>
+</body>
+</html>
