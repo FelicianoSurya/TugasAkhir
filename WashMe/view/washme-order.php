9fcc4c1ad270d4c4aa953632aa026d509a8c87e2
FelicianoSurya <suryamarcello@gmail.com>
2019-04-28 16:55:01 +0700

    .

--- WashMe/view/washme-order.php
+++ WashMe/view/washme-order.php
@@ -5,8 +5,8 @@
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"></script>
 	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>
-	<link rel="stylesheet" type="text/css" href="../asset/css/washme-order.css">
-	<title></title>
+	<link rel="stylesheet" type="text/css" href="../asset/css/washme-order1.css">
+	<title></title>	
 </head>
 <body>
 	<?php
@@ -24,7 +24,10 @@
 		</div>
 		<div class="padding flex">
 			<div class="text"><?php echo $name?>, <?php echo $alamat?></div>
-			<div class="border-mid"><img src="../asset/images/map.png" class="img"></div>
+			<div class="border-mid">
+				<div class="bg"></div>
+				<img src="../asset/images/map.png" class="img">
+			</div>
 		</div>
 		<div class="padding jarak">0.94 Km</div>
 		<hr class="hr">
