b2af650f05afaf0608ca62ef5d0e80e59a7190bc
David Leonardo <davidleonardo621@gmail.com>
2019-04-30 22:43:06 +0700

    .

---
+++ WashMe/view/washme-shopproduct.php
@@ -0,0 +1,70 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>WashMe Mobile</title>
+	<link rel="icon" href="../asset/images/logo.png">
+	<link rel="stylesheet" type="text/css" href="../asset/css/washme-shopproduct.css">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"></script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>
+</head>
+<body>
+<form>
+<div class="container-fluid-screen">
+	<div class="body">
+		<div class="header">
+			<div class="border">
+				<img src="../asset/images/left-arrow.png">
+			</div>
+			<div class="border">
+				<img src="../asset/images/shopping-cart1.png">
+			</div>
+		</div>
+		<div class="gambar">
+			<img src="../asset/images/setrika1.png">
+		</div>
+		<div class="text1">Setrika Listrik Silver Star Es300 R20</div>
+		<div class="harga">Rp 515.000</div>
+		<div class="flex">
+			<img src="../asset/images/star1.png" class="star">
+			<img src="../asset/images/star1.png" class="star">
+			<img src="../asset/images/star1.png" class="star">
+			<img src="../asset/images/star1.png" class="star">
+			<img src="../asset/images/star1.png" class="star">
+			<p class="harga">5</p>
+			<p class="text2">(200)</p>
+			<div class="garis"></div>
+			<p class="text2">400 Sold</p>
+		</div>
+		<div class="flex top">
+			<img src="../asset/images/truck.png" class="img1">
+			<p class="text3">Cost :</p>
+			<p class="text4">Rp 8.000</p>
+			<p class="text4">-</p>
+			<p class="text4">Rp17.000</p>
+		</div>
+		<div class="flex top">
+			<div class="flex border1">
+				<img src="../asset/images/hearts.png" class="img">
+				<div class="flex1">
+					<p class="text5">Like</p>
+					<p class="text5">117</p>
+				</div>
+			</div>
+			<div class="flex border1">
+				<img src="../asset/images/star3.png" class="img">
+				<div class="flex1">
+					<p class="text5">Ratings</p>
+					<p class="text5">54</p>
+				</div>
+			</div>
+			<div class="border2">
+				<img src="../asset/images/share2.png" class="img">
+			</div>
+		</div>
+	</div>
+</div>
+</form>
+</body>
+</html>
