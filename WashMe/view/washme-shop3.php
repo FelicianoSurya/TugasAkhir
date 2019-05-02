190214e177e60804c5365ad452f3789cb9ea0b71
David Leonardo <davidleonardo621@gmail.com>
2019-05-02 19:43:10 +0700

    .

---
+++ WashMe/view/washme-shop3.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>WashMe Mobile</title>
+	<link rel="icon" href="../asset/images/logo.png">
+	<link rel="stylesheet" type="text/css" href="../asset/css/washme-shop3.css">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"></script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>
+</head>
+<body>
+<form>
+	<div class="container-fluid-screen">
+		<div class="header">
+			<img src="../asset/images/left-arrow1.png" class="img">
+			<div class="header-title">
+				<p class="title">Shopping Cart</p>
+			</div>
+		</div>
+		<hr class="hr">
+		<div class="body">
+			<div class="flex">
+				<img src="../asset/images/laundry-ateng.jpg" class="img">
+				<p class="text">Mega Laundry</p>
+			</div>
+			<div class="flex top">
+				<img src="../asset/images/covermesincuci.png" class="img1">
+				<div class="left">
+					<p class="text2">Cover Mesin Cuci Hello Kitty (Satin Tebal, ...</p>
+					<input type="number" name="jlh" id="jlh" class="jumlah" placeholder="Jumlah">
+					<p class="harga">Rp 24.000</p>
+				</div>
+			</div>
+		</div>
+		<hr class="hr">
+		<div class="bawah">
+			<hr class="hr">
+			<div class="footer">
+				<div class="flex">
+					<img src="../asset/images/coupon2.png" class="img">
+					<p class="text">Voucher</p>
+					<input type="text" name="code" id="code" placeholder="Enter voucher code" class="kode">
+				</div>
+				<div class="flex-end">
+					<p class="total">Subtotal :</p>
+					<p class="harga">Rp 24.000</p>
+					<button class="button">Check Out</button>
+				</div>
+			</div>
+		</div>
+	</div>
+</form>
+</body>
+</html>
---
+++ WashMe/view/washme-viewshop.php
@@ -0,0 +1,108 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>WashMe Mobile</title>
+	<link rel="icon" href="../asset/images/logo.png">
+	<link rel="stylesheet" type="text/css" href="../asset/css/washme-viewshop1.css">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"></script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>
+</head>
+<body>
+<form>
+	<div class="container-fluid-screen">
+		<div class="header">
+			<div class="flex">
+				<img src="../asset/images/left-arrow.png" class="img">
+				<input type="text" name="search" id="search" placeholder="Search in Mega Laundry" class="border">
+			</div>
+			<p class="nama">Mega Laundry</p>
+			<p class="alamat">Jln Budi Karya No 15</p>
+		</div>
+		<div class="body">
+			<div class="flex1">
+				<div class="box">
+					<img src="../asset/images/detergencair.png" class="img">
+					<div class="content">
+						<p class="text">Detergen Cair Laundry Easy Clean (5 Liter)</p>
+						<p class="harga">Rp 35.000</p>
+						<div class="flex-end">
+							<img src="../asset/images/star1.png" class="star">
+							<img src="../asset/images/star1.png" class="star">
+							<img src="../asset/images/star1.png" class="star">
+							<img src="../asset/images/star1.png" class="star">
+							<img src="../asset/images/star1.png" class="star">
+							<p class="text1">141 sold</p>
+						</div>
+						<div class="flex-end top">
+							<img src="../asset/images/placeholder2.png" class="img1">
+							<p class="kota">Pontianak</p>
+						</div>
+					</div>
+				</div>
+				<div class="box">
+					<img src="../asset/images/covermesincuci.png" class="img">
+					<div class="content">
+						<p class="text">Cover Mesin cuci Hello Kity (Satin Tebal, Anti..)</p>
+						<p class="harga">Rp 24.000</p>
+						<div class="flex-end">
+							<img src="../asset/images/star1.png" class="star">
+							<img src="../asset/images/star1.png" class="star">
+							<img src="../asset/images/star1.png" class="star">
+							<img src="../asset/images/star1.png" class="star">
+							<img src="../asset/images/star1.png" class="star">
+							<p class="text1">46 sold</p>
+						</div>
+						<div class="flex-end top">
+							<img src="../asset/images/placeholder2.png" class="img1">
+							<p class="kota">Pontianak</p>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div class="flex1 top1">
+				<div class="box">
+					<img src="../asset/images/rinso.png" class="img">
+					<div class="content">
+						<p class="text">Rinso 1800gr / Rinso 1,8 kg / (Bubuk) Classic Fresh</p>
+						<p class="harga">Rp 38.000</p>
+						<div class="flex-end">
+							<img src="../asset/images/star1.png" class="star">
+							<img src="../asset/images/star1.png" class="star">
+							<img src="../asset/images/star1.png" class="star">
+							<img src="../asset/images/star1.png" class="star">
+							<img src="../asset/images/star1.png" class="star">
+							<p class="text1">150 sold</p>
+						</div>
+						<div class="flex-end top">
+							<img src="../asset/images/placeholder2.png" class="img1">
+							<p class="kota">Pontianak</p>
+						</div>
+					</div>
+				</div>
+				<div class="box">
+					<img src="../asset/images/mesincuci.png" class="img">
+					<div class="content">
+						<p class="text">Mito VM1 Mesin Cuci Mini Portable Low Watt 170 W</p>
+						<p class="harga">Rp 664.900</p>
+						<div class="flex-end">
+							<img src="../asset/images/star1.png" class="star">
+							<img src="../asset/images/star1.png" class="star">
+							<img src="../asset/images/star1.png" class="star">
+							<img src="../asset/images/star1.png" class="star">
+							<img src="../asset/images/star1.png" class="star">
+							<p class="text1">38 sold</p>
+						</div>
+						<div class="flex-end top">
+							<img src="../asset/images/placeholder2.png" class="img1">
+							<p class="kota">Pontianak</p>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</form>
+</body>
+</html>
