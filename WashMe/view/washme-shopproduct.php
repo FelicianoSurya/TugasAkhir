505e432d7ead09d2dac57819096dc17890874899
David Leonardo <davidleonardo621@gmail.com>
2019-05-03 00:42:05 +0700

    .

--- WashMe/view/washme-shopproduct.php
+++ WashMe/view/washme-shopproduct.php
@@ -3,7 +3,7 @@
 <head>
 	<title>WashMe Mobile</title>
 	<link rel="icon" href="../asset/images/logo.png">
-	<link rel="stylesheet" type="text/css" href="../asset/css/washme-shopproduct1.css">
+	<link rel="stylesheet" type="text/css" href="../asset/css/washme-shopproduct.css">
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"></script>
@@ -90,7 +90,116 @@
 			</div>
 		</div>
 	</div>
-	<div class="footer">
+	<div class="isi">
+		<hr class="hr">
+		<div class="body">
+			<div class="flex1">
+				<p class="text">Product Details</p>
+				<div class="flex">
+					<img src="../asset/images/clock2.png" class="img">
+					<p class="waktu">10 months</p>
+				</div>
+			</div>
+		</div>
+		<hr class="hr">
+		<div class="box">
+			<div class="flex">
+				<div class="box-in">
+					<p class="text">Categories</p>
+				</div>
+				<div class="box-in2">
+					<p class="text">Iron</p>
+				</div>
+			</div>
+			<hr class="hr2">
+			<div class="flex">
+				<div class="box-in">
+					<p class="text">Brand</p>
+				</div>
+				<div class="box-in2">
+					<p class="text">Silver Star</p>
+				</div>
+			</div>
+			<hr class="hr2">
+			<div class="flex">
+				<div class="box-in">
+					<p class="text">Stock</p>
+				</div>
+				<div class="box-in2">
+					<p class="text">523</p>
+				</div>
+			</div>
+			<hr class="hr2">
+		</div>
+		<div class="body1">
+			<p class="teks">Setrika Uap Silver Star</p>
+			<p class="teks top">Kapasitas air :3,5 liter</p>
+			<p class="teks">Daya : 1000 watt</p>
+			<p class="teks">Made in China</p>
+		</div>
+		<hr class="hr">
+		<div class="body2">
+			<div class="flex1">
+				<div>
+					<p class="text3">Product Ratings</p>
+					<div class="flex">
+						<img src="../asset/images/star1.png" class="star">
+						<img src="../asset/images/star1.png" class="star">
+						<img src="../asset/images/star1.png" class="star">
+						<img src="../asset/images/star1.png" class="star">
+						<img src="../asset/images/star1.png" class="star">
+						<p class="harga">5</p>
+						<p class="teks">(200)</p>
+					</div>
+				</div>
+				<div>
+					<p class="teks2">See All ></p>
+				</div>
+			</div>
+		</div>
+		<hr class="hr">
+		<div class="body2">
+			<div class="flex1">
+				<div class="flex3">
+					<img src="../asset/images/girl.png" class="img1">
+					<div>
+						<p class="text4">Eva</p>
+						<div class="flex">
+							<img src="../asset/images/star1.png" class="star1">
+							<img src="../asset/images/star1.png" class="star1">
+							<img src="../asset/images/star1.png" class="star1">
+							<img src="../asset/images/star1.png" class="star1">
+							<img src="../asset/images/star1.png" class="star1">
+						</div>
+						<p class="tanggal">01 - 02 - 2019</p>
+					</div>
+				</div>
+				<img src="../asset/images/like-grey.png" class="img2">
+			</div>
+		</div>
+		<hr class="hr">
+		<div class="body2">
+			<div class="flex1">
+				<div class="flex3">
+					<img src="../asset/images/girl.png" class="img1">
+					<div>
+						<p class="text4">Eva</p>
+						<div class="flex">
+							<img src="../asset/images/star1.png" class="star1">
+							<img src="../asset/images/star1.png" class="star1">
+							<img src="../asset/images/star1.png" class="star1">
+							<img src="../asset/images/star1.png" class="star1">
+							<img src="../asset/images/star1.png" class="star1">
+						</div>
+						<p class="komentar">Kualitas baik, sesuai dengan gambar, saya sangat suka dengan setrika ini</p>
+						<p class="tanggal">01 - 02 - 2019</p>
+					</div>
+				</div>
+				<img src="../asset/images/like-grey.png" class="img2">
+			</div>
+		</div>
+		<hr class="hr">
+		<div class="footer">
 		<div class="footer-box">
 			<div>
 				<img src="../asset/images/speech-bubble.png" class="image">
@@ -105,6 +214,7 @@
 		<div class="footer-box1">
 			<p class="teks">Buy Now</p>
 		</div>
+	</div>
 	</div>
 </div>
 </form>
