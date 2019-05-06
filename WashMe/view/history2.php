04058eab0a967b371f179ed6bedfcc29b2c0f018
David Leonardo <davidleonardo621@gmail.com>
2019-05-06 12:21:10 +0700

    .

---
+++ WashMe/view/history2.php
@@ -0,0 +1,95 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>WashMe Mobile</title>
+	<link rel="icon" href="../asset/images/logo.png">
+	<link rel="stylesheet" type="text/css" href="../asset/css/history2.css">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"></script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>
+</head>
+<body>
+<form>
+	<div class="container-fluid-screen">
+		<div class="header flex">
+			<img src="../asset/images/left-arrow2.png" class="arrow">
+			<div class="title">History</div>
+		</div>
+		<div class="box flex">
+			<img src="../asset/images/check.png" class="img">
+			<div class="box-in">
+				<div class="flex-between"">
+					<p class="text">Order</p>
+					<p class="text1">Saturday, 23 March 2019, 12:44</p>
+				</div>
+				<div class="flex-between">
+					<p class="text">Received</p>
+					<p class="text1">Order WMO-1234566</p>
+				</div>
+			</div>
+		</div>
+		<hr class="hr">
+		<div class="box flex-mid">
+			<img src="../asset/images/star1.png" class="star">
+			<img src="../asset/images/star1.png" class="star">
+			<img src="../asset/images/star1.png" class="star">
+			<img src="../asset/images/star1.png" class="star">
+			<img src="../asset/images/star1.png" class="star">
+		</div>
+		<hr class="hr">
+		<div class="box flex">
+			<img src="../asset/images/driver.png" class="img2">
+			<div class="box-in">
+				<p class="text">Budi</p>
+				<p class="text2">085245837878</p>
+			</div>
+		</div>
+		<hr class="hr">
+		<div class="box">
+			<p class="title2">Location Details</p>
+			<div class="flex box-in2">
+				<img src="../asset/images/destination.png" class="img3">
+				<div class="box-in">
+					<div>
+						<p>Home</p>
+						<p>Jln Veteran gg Syukur1 No 50 D</p>
+					</div>
+					<div class="top">
+						<p>Mega Laundry</p>
+						<p>Jln Budi Karya No 15</p>
+					</div>
+				</div>
+			</div>
+		</div>
+		<hr class="hr">
+		<div class="box">
+			<p class="title2">Order Details</p>
+			<div class="box-in3">
+				<div class="flex-between">
+					<p>(2 Kg) Wash & Iron</p>
+					<p>Rp 10.000</p>
+				</div>
+				<div class="flex-between">
+					<p>(2 Pcs) Dry Cleaning</p>
+					<p>Rp 25.000</p>
+				</div>
+				<div class="flex-between">
+					<p>Extra</p>
+					<p>Rp 19.000</p>
+				</div>
+				<div class="flex-between">
+					<p>Fee</p>
+					<p>Rp 3.000</p>
+				</div>
+			</div>
+		</div>
+		<hr class="hr">
+		<div class="box1 flex-between">
+			<p>Total Price</p>
+			<p>Rp 57.000</p>
+		</div>
+	</div>
+</form>
+</body>
+</html>
---
+++ WashMe/view/termsofservice.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>WashMe Mobile</title>
+	<link rel="icon" href="../asset/images/logo.png">
+	<link rel="stylesheet" type="text/css" href="../asset/css/termsofservice.css">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"></script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>
+</head>
+<body>
+
+</body>
+</html>
