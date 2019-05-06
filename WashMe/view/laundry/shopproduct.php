5fa5e04d0f45df9505f56699a8cf441be2129e11
David Leonardo <davidleonardo621@gmail.com>
2019-05-06 22:56:30 +0700

    .

---
+++ WashMe/view/laundry/shopproduct.php
@@ -0,0 +1,102 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>WashMe Mobile</title>
+	<link rel="icon" href="../../asset/images/logo.png">
+	<link rel="stylesheet" type="text/css" href="../../asset/css/laundry/shopproduct.css">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"></script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>
+</head>
+<body>
+<form>
+	<div class="container-fluid-screen">
+	<div class="body">
+		<div class="flex header">
+			<div class="title">Add Product</div>
+			<div class="submit">SUBMIT</div>
+		</div>
+		<div class="box">
+			<img src="../../asset/images/covermesincuci.png" class="img1">
+		</div>
+		<hr class="hr">
+		<div class="box">
+			<p class="text">Product Name</p>
+		</div>
+		<hr class="hr">
+		<div class="box">
+			<p class="text">Product Description / #hashtags</p>
+		</div>
+		<hr class="hr">
+		<div class="box flex">
+			<img src="../../asset/images/list.png" class="img">
+			<div class="box-in">
+				<p>Category</p>
+				<img src="../../asset/images/right-arrow.png" class="img2">
+			</div>
+		</div>
+		<hr class="hr">
+		<div class="box flex">
+			<img src="../../asset/images/tag.png" class="img">
+			<div class="box-in">
+				<p>Price</p>
+				<p>Rp</p>
+			</div>
+		</div>
+		<hr class="hr">
+		<div class="box flex">
+			<img src="../../asset/images/package.png" class="img">
+			<div class="box-in">
+				<p>Stock</p>
+				<p class="text2">Set Stock</p>
+			</div>
+		</div>
+		<hr class="hr">
+		<div class="box flex">
+			<img src="../../asset/images/scale.png" class="img">
+			<div class="box-in">
+				<p>Weight</p>
+				<p class="text2">Set Weight</p>
+			</div>
+		</div>
+		<hr class="hr">
+		<div class="box flex">
+			<img src="../../asset/images/parking.png" class="img">
+			<div class="box-in">
+				<p>Shipping Fee</p>
+				<p>Rp</p>
+			</div>
+		</div>
+		<hr class="hr">
+		<div class="footer">
+			<a href="home.php">
+				<div class="box4">
+					<img src="../../asset/images/home.png" class="img2">
+					<div class="text">Home</div>
+				</div>
+			</a>
+			<a href="myorders-1.php">
+				<div class="box4">
+					<img src="../../asset/images/shopping-list2.png" class="img2">
+					<div class="text1">Orders</div>
+				</div>
+			</a>
+			<a href="washmeshop.php">
+				<div class="box4">
+					<img src="../../asset/images/shopping-cart2.png" class="img2">
+					<div class="text1">WashMe Shop</div>
+				</div>
+			</a>
+			<a href="myaccount.php">
+				<div class="box4">
+					<img src="../../asset/images/profile2.png" class="img2">
+					<div class="text1">Account</div>
+				</div>
+			</a>
+		</div>
+	</div>
+	</div>
+</form>
+</body>
+</html>
