94dd16eb797de80046a31cdb7bb02cb0ecbe0d1c
David Leonardo <davidleonardo621@gmail.com>
2019-05-07 18:52:21 +0700

    .

--- WashMe/view/laundry/myorders.php
+++ WashMe/view/laundry/myorders.php
@@ -1,9 +1,111 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title></title>
+	<title>WashMe Mobile</title>
+	<link rel="icon" href="../../asset/images/logo.png">
+	<link rel="stylesheet" type="text/css" href="../../asset/css/laundry/myorders1.css">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"></script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>
 </head>
 <body>
-
+<form>
+	<div class="container-fluid-screen">
+		<div class="header">My Orders</div>
+		<div class="body">
+			<div class="flex1">
+				<div class="box">
+					<div class="flex-end"><button class="button">Details</button></div>
+					<img src="../../asset/images/girl.png" class="img">
+					<div class="box-in">
+						<p class="text">Fleren Picestelia</p>
+						<p class="text">Jln Veteran gg syukur 1 no 50D</p>
+						<p class="text">Thursday, 2 May 2019</p>
+						<p class="text">-</p>
+						<p class="text">Friday, 3 May 2019</p>
+					</div>
+					<div class="flex">
+						<button class="button1">Accept</button>
+						<button class="button2">Reject</button>
+					</div>
+				</div>
+				<div class="box">
+					<div class="flex-end"><button class="button">Details</button></div>
+					<img src="../../asset/images/girl.png" class="img">
+					<div class="box-in">
+						<p class="text">Fleren Picestelia</p>
+						<p class="text">Jln Veteran gg syukur 1 no 50D</p>
+						<p class="text">Thursday, 2 May 2019</p>
+						<p class="text">-</p>
+						<p class="text">Friday, 3 May 2019</p>
+					</div>
+					<div class="flex">
+						<button class="button1">Accept</button>
+						<button class="button2">Reject</button>
+					</div>
+				</div>
+			</div>
+			<div class="flex1 top">
+				<div class="box">
+					<div class="flex-end"><button class="button">Details</button></div>
+					<img src="../../asset/images/girl.png" class="img">
+					<div class="box-in">
+						<p class="text">Fleren Picestelia</p>
+						<p class="text">Jln Veteran gg syukur 1 no 50D</p>
+						<p class="text">Thursday, 2 May 2019</p>
+						<p class="text">-</p>
+						<p class="text">Friday, 3 May 2019</p>
+					</div>
+					<div class="flex">
+						<button class="button1">Accept</button>
+						<button class="button2">Reject</button>
+					</div>
+				</div>
+				<div class="box">
+					<div class="flex-end"><button class="button">Details</button></div>
+					<img src="../../asset/images/girl.png" class="img">
+					<div class="box-in">
+						<p class="text">Fleren Picestelia</p>
+						<p class="text">Jln Veteran gg syukur 1 no 50D</p>
+						<p class="text">Thursday, 2 May 2019</p>
+						<p class="text">-</p>
+						<p class="text">Friday, 3 May 2019</p>
+					</div>
+					<div class="flex">
+						<button class="button1">Accept</button>
+						<button class="button2">Reject</button>
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="footer">
+			<a href="home.php">
+				<div class="box4">
+					<img src="../../asset/images/home2.png" class="img2">
+					<div class="text1">Home</div>
+				</div>
+			</a>
+			<a href="myorders-1.php">
+				<div class="box4">
+					<img src="../../asset/images/shopping-list.png" class="img2">
+					<div class="text">Orders</div>
+				</div>
+			</a>
+			<a href="washmeshop.php">
+				<div class="box4">
+					<img src="../../asset/images/shopping-cart2.png" class="img2">
+					<div class="text1">WashMe Shop</div>
+				</div>
+			</a>
+			<a href="myaccount.php">
+				<div class="box4">
+					<img src="../../asset/images/profile2.png" class="img2">
+					<div class="text1">Account</div>
+				</div>
+			</a>
+		</div>
+	</div>
+</form>
 </body>
 </html>
--- WashMe/view/laundry/shopproduct.php
+++ WashMe/view/laundry/shopproduct.php
@@ -3,7 +3,7 @@
 <head>
 	<title>WashMe Mobile</title>
 	<link rel="icon" href="../../asset/images/logo.png">
-	<link rel="stylesheet" type="text/css" href="../../asset/css/laundry/shopproduct.css">
+	<link rel="stylesheet" type="text/css" href="../../asset/css/laundry/shopproduct1.css">
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"></script>
@@ -22,18 +22,24 @@
 		</div>
 		<hr class="hr">
 		<div class="box">
-			<p class="text">Product Name</p>
+			<input type="text" name="name" id="name" placeholder="Product Name" class="text">
 		</div>
 		<hr class="hr">
 		<div class="box">
-			<p class="text">Product Description / #hashtags</p>
+			<input type="text" name="name" id="name" placeholder="Product Description / #hashtags" class="text">
 		</div>
 		<hr class="hr">
 		<div class="box flex">
 			<img src="../../asset/images/list.png" class="img">
 			<div class="box-in">
 				<p>Category</p>
-				<img src="../../asset/images/right-arrow.png" class="img2">
+				<div style="display: flex; justify-content: flex-end; position: relative; width: 100%; margin-top: -100px;">
+					<select style="position: absolute; padding: 20px 40px;">
+						<option>sd</option>
+						<option>asd</option>
+					</select>
+					<img src="../../asset/images/right-arrow.png" class="img2" style="position: absolute; right: 0px; margin-top: 10px;">
+				</div>
 			</div>
 		</div>
 		<hr class="hr">
@@ -41,7 +47,10 @@
 			<img src="../../asset/images/tag.png" class="img">
 			<div class="box-in">
 				<p>Price</p>
-				<p>Rp</p>
+				<div class="flex-end">
+					<p>Rp</p>
+					<input type="number" name="name" id="name" placeholder="" class="text3">
+				</div>
 			</div>
 		</div>
 		<hr class="hr">
@@ -49,7 +58,7 @@
 			<img src="../../asset/images/package.png" class="img">
 			<div class="box-in">
 				<p>Stock</p>
-				<p class="text2">Set Stock</p>
+				<input type="number" name="name" id="name" placeholder="Set Stock" class="text2">
 			</div>
 		</div>
 		<hr class="hr">
@@ -57,7 +66,7 @@
 			<img src="../../asset/images/scale.png" class="img">
 			<div class="box-in">
 				<p>Weight</p>
-				<p class="text2">Set Weight</p>
+				<input type="number" name="name" id="name" placeholder="Set Weight" class="text2">
 			</div>
 		</div>
 		<hr class="hr">
@@ -65,15 +74,18 @@
 			<img src="../../asset/images/parking.png" class="img">
 			<div class="box-in">
 				<p>Shipping Fee</p>
-				<p>Rp</p>
+				<div class="flex-end">
+					<p>Rp</p>
+					<input type="number" name="name" id="name" placeholder="" class="text3">
+				</div>
 			</div>
 		</div>
 		<hr class="hr">
 		<div class="footer">
 			<a href="home.php">
 				<div class="box4">
-					<img src="../../asset/images/home.png" class="img2">
-					<div class="text">Home</div>
+					<img src="../../asset/images/home2.png" class="img2">
+					<div class="text1">Home</div>
 				</div>
 			</a>
 			<a href="myorders-1.php">
@@ -84,8 +96,8 @@
 			</a>
 			<a href="washmeshop.php">
 				<div class="box4">
-					<img src="../../asset/images/shopping-cart2.png" class="img2">
-					<div class="text1">WashMe Shop</div>
+					<img src="../../asset/images/shopping-cart.png" class="img2">
+					<div class="text">WashMe Shop</div>
 				</div>
 			</a>
 			<a href="myaccount.php">
