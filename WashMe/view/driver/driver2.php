c7d56a9396185dedf3fbcdbd77f95350bd870637
David Leonardo <davidleonardo621@gmail.com>
2019-05-15 22:59:06 +0700

    .

---
+++ WashMe/view/driver/driver2.php
@@ -0,0 +1,100 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>WashMe Mobile</title>
+	<link rel="icon" href="../../asset/images/logo.png">
+	<link rel="stylesheet" type="text/css" href="../../asset/css/driver/driver2.css">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"></script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>
+</head>
+<body>
+<form>
+	<div class="container-fluid-screen">
+		<div class="header">
+			<img src="../../asset/images/left-arrow1.png">
+			<p>My Orders</p>
+		</div>
+		<div class="box">
+			<p>Ordes WMO-1234567</p>
+			<img src="../../asset/images/girl.png">
+		</div>
+		<div class="box2">
+			<div class="flex">
+				<p class="text1">Name</p>
+				<p class="text2">:</p>
+				<p class="text3">Fleren Picestelia</p>
+			</div>
+			<div class="flex">
+				<p class="text1">Phone</p>
+				<p class="text2">:</p>
+				<p class="text3">08992959698</p>
+			</div>
+			<div class="box-in">
+				<div class="border1">
+					<p class="title">Pick Up</p>
+					<div class="border-in">
+						<p class="text1">Date</p>
+						<p class="text2">Sunday, 12 May 2019</p>
+					</div>
+					<div class="border-in">
+						<p class="text1">Time</p>
+						<p class="text2">10:00 AM - 11:00 AM</p>
+					</div>
+					<div class="border-in">
+						<p class="text1">Address</p>
+						<p class="text2">Jln Veteran gg Syukur 1 No 50D</p>
+					</div>
+				</div>
+				<div class="border1">
+					<p class="title">Delivery</p>
+					<div class="border-in">
+						<p class="text1">Date</p>
+						<p class="text2">Monday, 13 May 2019</p>
+					</div>
+					<div class="border-in">
+						<p class="text1">Time</p>
+						<p class="text2">1:00 PM - 2:00 PM</p>
+					</div>
+					<div class="border-in">
+						<p class="text1">Address</p>
+						<p class="text2">Jln Veteran gg Syukur 1 No 50D</p>
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="box2">
+			<div class="box-in1">
+				<p class="teks">Laundry Detail</p>
+				<button>Edit</button>
+			</div>
+			<div class="content">
+				<div class="content-in">
+					<p>(3 Kg) Wash & Fold</p>
+					<p>Rp 15.000</p>
+				</div>
+				<div class="content-in">
+					<p>(2 Pcs) Dry Cleaning</p>
+					<p>Rp 25.000</p>
+				</div>
+				<div class="content-in">
+					<p>Extra</p>
+					<p>Rp 19.000</p>
+				</div>
+				<div class="content-in">
+					<p>Fee</p>
+					<p>Rp 3.000</p>
+				</div>
+			</div>
+		</div>
+		<div class="box2">
+			<div class="box3">
+				<p>Total Price</p>
+				<p>Rp 31.000</p>
+		</div>
+		</div>
+	</div>
+</form>
+</body>
+</html>
